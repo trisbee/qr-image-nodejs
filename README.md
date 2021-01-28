11b6c56c8077678f94b07d5890c3d40ebc4ff928
HamAndRock <jakub@havelka.net>
2021-01-28 16:56:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# QR Image
+Simple library allowing you to generate an image in middle of an QR code.
+
+This library contains only 1 method and that is `generateQRWithImage`
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import QRCOde = require('qrcode');
+import {QRCodeToBufferOptions} from "qrcode";
+
+
+const {createCanvas, loadImage} = require('canvas')
+
+
+
+
+
+export const generateQRWithImage = async (qrCodeContent: string, width: number, margin: number, imageBuffer?: Buffer | null, options?: QRCodeToBufferOptions) : Promise<{
+    buffer: Buffer,
+    qrImageCoverage: number
+}> => {
+
+    const fullHeight = width + margin;
+
+    const canvas = createCanvas(fullHeight, fullHeight)
+    const ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, fullHeight, fullHeight);
+
+    if (options) {
+        options.width = width;
+        options.margin = 0;
+    }
+
+
+    const qrBuffer = await QRCOde.toBuffer(qrCodeContent, options || {
+        errorCorrectionLevel: imageBuffer ? 'H' : 'M',
+        width: width,
+        margin: 0,
+    })
+
+    ctx.drawImage(await loadImage(qrBuffer), margin / 2, margin / 2)
+
+    let imageVolume = 0;
+
+    if (imageBuffer) {
+        let image = await loadImage(imageBuffer);
+        imageVolume = image.width * image.height
+
+        let dx = (width / 2 - image.width / 2) + margin / 2;
+        ctx.drawImage(image, dx, dx)
+    }
+
+
+
+    const qrVolume = width * width;
+    const result = (imageVolume / qrVolume) * 100;
+
+
+    return {
+        buffer: canvas.toBuffer(),
+        qrImageCoverage: result,
+    }
+
+
+}
