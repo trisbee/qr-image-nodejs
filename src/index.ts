2de1e86e0b38509204ee90033b2f87ac6743ff8b
Michal Landsman <landsman@insuit.cz>
2022-03-07 17:57:49 +0100

    comments, code style

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,69 @@
 import QRCOde = require('qrcode');
 import {QRCodeToBufferOptions} from "qrcode";
-
-
-const {createCanvas, loadImage} = require('canvas')
+import {createCanvas, loadImage} from "canvas";
 
 interface QRResponse {
     buffer: Buffer,
     coverage: number
 }
 
+export async function generateQRWithImage(
+    qrCodeContent: string,
+    width: number,
+    margin: number,
+    imageBuffer?: Buffer | null,
+    options?: QRCodeToBufferOptions
+): Promise<QRResponse> {
 
-export const generateQRWithImage = async (qrCodeContent: string, width: number, margin: number, imageBuffer?: Buffer | null, options?: QRCodeToBufferOptions): Promise<QRResponse> => {
+    /** we want's to create a square, so this is size of one of the side */
+    const squareSideSize = width + margin;
 
-    const fullHeight = width + margin;
+    /** init new canvas */
+    const canvas = createCanvas(squareSideSize, squareSideSize);
 
-    const canvas = createCanvas(fullHeight, fullHeight)
-    const ctx = canvas.getContext('2d')
+    /** create context for drawing on canvas */
+    const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, fullHeight, fullHeight);
+    ctx.fillRect(0, 0, squareSideSize, squareSideSize);
 
     if (options) {
         options.width = width;
         options.margin = 0;
     }
 
-
+    /** Returns a Buffer containing a representation of the QR Code image. Only works with png format */
     const qrBuffer = await QRCOde.toBuffer(qrCodeContent, options || {
         errorCorrectionLevel: imageBuffer ? 'H' : 'M',
         width: width,
         margin: 0,
     })
 
-    ctx.drawImage(await loadImage(qrBuffer), margin / 2, margin / 2)
+    /** put prepared QR code to the canvas */
+    ctx.drawImage(await loadImage(qrBuffer), margin / 2, margin / 2);
 
+    /** @todo: what is this? */
     let imageVolume = 0;
 
+    /** if we want to put image inside the QR code we will draw it above it, to middle of the square */
     if (imageBuffer) {
         let image = await loadImage(imageBuffer);
-        imageVolume = image.width * image.height
+        imageVolume = image.width * image.height;
 
+        /** @todo: what does it means, doing? */
         let dx = (width / 2 - image.width / 2) + margin / 2;
-        ctx.drawImage(image, dx, dx)
+
+        /** draw our image above the QR code */
+        ctx.drawImage(image, dx, dx);
     }
 
-
+    /** @todo: why is this operation necessary here */
     const qrVolume = width * width;
-    const result = (imageVolume / qrVolume) * 100;
 
+    /** @todo: why is this operation necessary here */
+    const result = (imageVolume / qrVolume) * 100;
 
     return {
         buffer: canvas.toBuffer(),
         coverage: result,
     }
-
-
 }
